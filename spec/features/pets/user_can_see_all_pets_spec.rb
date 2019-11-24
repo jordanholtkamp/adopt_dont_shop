365c7923cfec54ad75763c6b0418c8369233cd3b
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-11-23 19:48:13 -0700

    Finish functionality to show pet

--- spec/features/pets/user_can_see_all_pets_spec.rb
+++ spec/features/pets/user_can_see_all_pets_spec.rb
@@ -4,8 +4,8 @@ describe 'as a visitor' do
   before :each do
     @otter_shelter = Shelter.create(name: "Get Otter Here", address: "789 River Street", city: "Lakewood", state: "CO", zip: 80213)
     @cat_shelter = Shelter.create(name: "The Cat's Pajamas", address: "123 Kitty Cat Street", city: "Denver", state: "CO", zip: 80211)
-    @steve = @otter_shelter.pets.create(image: "https://cdn.mainichi.jp/vol1/2018/05/12/20180512p2a00m0na022000p/9.jpg?4", name: 'Steve', approximate_age: 4, sex: 'Male')
-    @karen = @cat_shelter.pets.create(image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuBI2oxO36Tx3myc68n_Dc_wKAVX3ffuEF3-JZR02-Ifh-uwho8w&s", name: 'Karen', approximate_age: 5, sex: 'Female')
+    @steve = @otter_shelter.pets.create(image: "https://cdn.mainichi.jp/vol1/2018/05/12/20180512p2a00m0na022000p/9.jpg?4", name: 'Steve', approximate_age: 4, sex: 'Male', description: "I like to swim and eat potatoes!", status:"Adoptable")
+    @karen = @cat_shelter.pets.create(image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuBI2oxO36Tx3myc68n_Dc_wKAVX3ffuEF3-JZR02-Ifh-uwho8w&s", name: 'Karen', approximate_age: 5, sex: 'Female', description: "I like to sit on laps and judge people.", status: "Adoptable")
   end
 
   describe 'when I am on the pets index page' do
@@ -41,17 +41,3 @@ describe 'as a visitor' do
     end
   end
 end
-
-
-
-# User Story 9, Pet Show
-#
-# As a visitor
-# When I visit '/pets/:id'
-# Then I see the pet with that id including the pet's:
-# - image
-# - name
-# - description
-# - approximate age
-# - sex
-# - adoptable/pending adoption status
